2fc917cab5e769a7cf7b3761f81e1a1a412c8b81
tflynn93 <tflynn93@gmail.com>
2014-11-04 13:47:33 -0500

    Filled out Character class
    Added methods and variables to Character class

--- src/Character.java
+++ src/Character.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JButton;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,6 +10,84 @@
  *
  * @author Cameron
  */
-public class Character {
+public class Character 
+{    
+    JButton character;
+    int width;
+    int height;
+    int speed;
+    int x;
+    int y;
     
+    public void character(int width, int height, int speed, int x, int y)
+    {
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+        this.x = x;
+        this.y = y;
+        this.character = new JButton();
+    }
+    
+    public void fireProjectile(int speed, int width, int direction, Projectile p)
+    {
+        //Insert projectile firing code here
+    }
+    
+    public void destroy()
+    {
+        //destroy character here
+    }
+    
+    public void setHeight(int height)
+    {
+        this.height = height;
+    }
+    
+    public int getHeight()
+    {
+        return this.height;
+    }
+    
+    public void setWidth(int width)
+    {
+        this.width = width;
+    }
+    
+    public int getWidth()
+    {
+        return this.width;
+    }
+    
+    public void setSpeed(int speed)
+    {
+        this.speed = speed;
+    }
+    
+    public int getSpeed()
+    {
+        return this.speed;
+    }
+    
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+    
+    public int getX()
+    {
+        return this.x;
+    }
+    
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+    
+    public int getY()
+    {
+        return this.y;
+    }
+
 }
+
