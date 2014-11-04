d7f6ec3a5138580722913e0c5e4e9da30f4adc6f
Shane Eckenrode <sue2@psu.edu>
2014-11-04 13:31:32 -0500

    Update

--- src/app.java
+++ src/app.java
@@ -1,19 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-/**
- *
- * @author Cameron
- */
 public class app {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
