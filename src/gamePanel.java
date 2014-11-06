929e1b082902661acd80ea152f2e446c6fc592e1
Cameron Runyeon <Cameron@CameronMacbook-3.local>
2014-11-06 13:16:00 -0500

    Start button now works

--- src/gamePanel.java
+++ src/gamePanel.java
@@ -1,5 +1,8 @@
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /*
@@ -11,14 +14,27 @@ import javax.swing.JPanel;
  *
  * @author Cameron
  */
-public class gamePanel extends JPanel {
+public class gamePanel extends JPanel implements ActionListener {
     startPanel stp;
+    Character char1 = new Character();
+    
     
     public gamePanel(){
-        
+        super();
         setLayout(new BorderLayout());
         stp = new startPanel();
-        stp.add(stp.startButton);
         this.add(stp, "North");
+        stp.startButton.addActionListener(this);
+       
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        Object obj = e.getSource();
+        
+        if (obj==stp.startButton)   {
+            char1.character = new JButton("Character");
+            add(char1.character);
+            
+        }
     }
 }
